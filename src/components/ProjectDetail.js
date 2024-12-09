979a57d3f253423cec2e3ed1c9e4975815491fde
Karan <48997539+karanb67@users.noreply.github.com>
2024-12-09 09:41:33 -0500

    Update ProjectDetail.js

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -40,7 +40,7 @@ const ProjectDetail = () => {
           <h2 className="text-3xl font-semibold mb-4 text-gray-900">Demo Video</h2>
           <div className="rounded-lg shadow-lg overflow-hidden">
           <iframe
-           src="https://drive.google.com/file/d/1IVkrahjhW0F7riUjIHBESE9ORKjz-KC1/preview"
+           src="https://drive.google.com/file/d/1qroEIG0Ok032SmIHFCLGgy_i5m54mCTj/view?usp=sharing"
            width="100%"
            height="480"
            allow="autoplay"
